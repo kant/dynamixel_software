eef9f562c2ab8fa31c249a8d44719b211dbaf739
Tim Übelhör <t.uebelhoer@irt.rwth-aachen.de>
2020-03-26 16:53:45 +0100

    Set min and max position using the user button

--- main.ino
+++ main.ino
@@ -9,13 +9,14 @@ const float DXL_PROTOCOL_VERSION = 2.0;
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 
 // motion parameters
-float zero_pos = 0;
-const float max_pos = 210;
+float min_pos = 0;
+float max_pos = 0;
 const float rpm = 15;
 const float omega_millis = 2 * PI * 15 / 60 * 1e-3;
 
 /**
- * Establishes the connunication to the motor. If already connected, nothing happens.
+ * Establishes the connunication to the motor. If already connected, nothing
+ * happens.
  */
 void init_dxl() {
   if (dxl.ping(DXL_ID)) {
@@ -29,33 +30,57 @@ void init_dxl() {
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
   DEBUG_SERIAL.println("success!");
 
-  // switch to current control
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_CURRENT);
-  dxl.torqueOn(DXL_ID);
-
-  // init position
-  DEBUG_SERIAL.println("homing");
-  dxl.setGoalCurrent(DXL_ID, 1, UNIT_PERCENT);
-  delay(1000);
-  zero_pos = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
-
-  // switch to postion control
+  // switch to postion control, defensive inital, min & max position
+  float position = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
+  min_pos = position;
+  max_pos = position;
+  dxl.setGoalPosition(DXL_ID, position, UNIT_DEGREE);
   dxl.torqueOff(DXL_ID);
   dxl.setOperatingMode(DXL_ID, OP_CURRENT_BASED_POSITION);
   dxl.torqueOn(DXL_ID);
   DEBUG_SERIAL.println("ready");
 }
 
-void setup() { DEBUG_SERIAL.begin(115200); }
+/**
+ * Checks if the button is pressed, set limits with disabled torque.
+ */
+void update_limits() {
+  // read the state of the pushbutton value:
+  int buttonState = digitalRead(BOARD_BUTTON_PIN);
+  float position = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
+  if (buttonState == HIGH) {
+    // disable torque and set limits
+    dxl.torqueOff(DXL_ID);
+    if (position < min_pos) {
+      min_pos = position;
+      DEBUG_SERIAL.printf("minimal postion updated to %f\n", position);
+    }
+    if (position > max_pos) {
+      max_pos = position;
+      DEBUG_SERIAL.printf("maximal postion updated to %f\n", position);
+    }
+  }
+  if (buttonState == LOW) {
+    dxl.torqueOn(DXL_ID);
+  }
+}
+
+void setup() {
+  DEBUG_SERIAL.begin(115200);
+  // Use button as input
+  pinMode(BOARD_BUTTON_PIN, INPUT_PULLDOWN);
+}
 
 void loop() {
   // reconnect motor if not connected
   init_dxl();
+  // set limits when button is pressed
+  update_limits();
 
   // Limit torque
   dxl.setGoalCurrent(DXL_ID, 25, UNIT_PERCENT);
-  // TODO handle overflows
-  float goal_pos = zero_pos + max_pos * (1 + sin(millis() * omega_millis)) * 0.5;
+  // TODO handle overflows after 50 days
+  float goal_pos = 0.5 * (min_pos + max_pos) +
+                   0.5 * (max_pos - min_pos) * sin(millis() * omega_millis);
   dxl.setGoalPosition(DXL_ID, goal_pos, UNIT_DEGREE);
 }
